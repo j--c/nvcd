7d46a7acc48bacc3f639ddf3f978a022beb760b5
j--c <johnmichaelcollins@protonmail.com>
2024-10-19 17:01:43 -0400

    Updated the command names to be better

--- plugin/nvcd.lua
+++ plugin/nvcd.lua
@@ -1,8 +1,7 @@
 vim.api.nvim_create_user_command(
-    "SwapNvimConfig",
+    "SwapCwdNvimConfig",
     function()
-        require 'nvcd'.swap_working_dir({
-            cncl_tkn = '12093uajn',
+        require 'nvcd'.swap_working_dir_absolute({
             new_wd = '~/.config/nvim'
         })
     end,
@@ -10,10 +9,9 @@ vim.api.nvim_create_user_command(
 )
 
 vim.api.nvim_create_user_command(
-    "SwapNvimCustom",
+    "SwapCwdCustom",
     function()
-        require 'nvcd'.swap_working_dir({
-            cncl_tkn = '12093uajn',
+        require 'nvcd'.swap_working_dir_absolute({
             new_wd = nil
         })
     end,
